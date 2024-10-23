53607f4279a85d0fb81cace7102bf4be706d9b48
Sule Balaban <127023712+sulebalaban@users.noreply.github.com>
2024-10-23 13:26:39 +0200

    Update points_calculation.sql

--- points_calculation.sql
+++ points_calculation.sql
@@ -1,3 +1,4 @@
+##If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have?##
 SELECT sales.customer_id,SUM(CASE 
 WHEN menu.product_name='sushi' THEN menu.price*20
 ELSE menu.price*10
